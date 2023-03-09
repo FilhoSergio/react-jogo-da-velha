822a4b13ac75e2deaa3b641f92a41bf6b61e46ee
filhosergio <serginhoadeus@gmail.com>
2023-03-09 14:48:26 -0300

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
         <p>
           OK OK
         </p>
-       
       </header>
     </div>
   );
